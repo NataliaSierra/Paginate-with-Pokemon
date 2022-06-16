1fe9d9e841c91cfd3ffd9a412f2d4fea8c25d4cd
N_Sierra <nataliasierra0404@gmail.com>
2022-06-16 09:12:26 -0500

    SUbiendo cambios

---
+++ src/Data/Pokemons.js
@@ -0,0 +1,38 @@
+export const Pokemons = [
+  {
+    name: 'Ditto - ADSI',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
+  },
+  {
+    name: 'Pikachu - ADSI',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+  },
+  {
+    name: 'Bulbasaur - ADSI',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  },
+  {
+    name: 'Charizard - ADSO',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+  },
+  {
+    name: 'Squirtle - ADSO',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+  },
+  {
+    name: 'Pidgeotto - ADSO',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png',
+  },
+  {
+    name: 'Meowth - ADSO',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/52.png',
+  },
+  {
+    name: 'Psyduck - ADSO',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/54.png',
+  },
+  {
+    name: 'Exeggcute - ADSI',
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/102.png',
+  },
+];
